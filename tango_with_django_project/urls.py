a61b230af6fe45cfa4b5161ba8436ada1e3a3e56
Pedro Belfort <belfortdev@gmail.com>
2020-01-29 00:06:38 +0000

    Rename project to comply with unit tests

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,4 @@
-"""it_tango URL Configuration
+"""tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
